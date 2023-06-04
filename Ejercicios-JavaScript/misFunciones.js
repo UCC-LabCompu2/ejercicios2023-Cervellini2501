64c8155c85a6c49e8a5791e29fb811fe06ce2898
Cervellini2501 <2200169@ucc.edu.ar>
2023-06-04 11:33:37 -0300

    se agrego ejercicio minipaint

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -152,3 +152,31 @@ function dibujarCirCuad(){
   ctx.fillStyle = "#8b4c99";
   ctx.fill();
 }
+
+
+var bandera;
+function dibujar(event){
+  var canvas = document.getElementById("canvasAdibujar");
+  var ctx= canvas.getContext("2d");
+
+  var posX = event.clientX;
+  var posY = event.clientY;
+  console.log(posX,posY);
+
+  canvas.onmousedown = function (){bandera=true};
+  canvas.onmouseup = function (){bandera=false};
+
+  if (bandera){
+    ctx.fillRect(posX,posY, 5,5);
+    ctx.fill;
+  }
+
+}
+
+function limpiarCanvas(){
+  var canvas = document.getElementById("canvasAdibujar");
+  var ctx= canvas.getContext("2d");
+
+  canvas.width = canvas.width;
+
+}
